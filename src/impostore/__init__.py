92cc0bd1c4c84b18ed9fca5bc2607fbeb8cbc6df
FrancescoPapalini <francescopapalini5@gmail.com>
2026-02-23 09:54:28 +0100

    Update __init__.py

--- src/impostore/__init__.py
+++ src/impostore/__init__.py
@@ -13,32 +13,51 @@ def main() -> None:
 
 
 
-    #=======================================
-    # impostazioni codice iniziale
-    #========================================
+    #=====================================================================
+    # IMPOSTAZIONE DEL CODICE --> variabili necessarie per tutto il codice
+    #=====================================================================
 
     import pygame
     
     # per iniziare pygame (come per accedere alle funzioni presenti in pygame)
     pygame.init()
+    
+    altezza_schermata = 800
+    lunghezza_schermata = 800
+    
+    # definizione dei font nella schermata di gioco 
+    font_titolo = pygame.font.SysFont('Calibri', 70)
+    font_normale = pygame.font.SysFont('Calibri', 30)
+    
+    #=========================================
+    # GENERARE UNA SCHERMATA --> gestione 
+    #=========================================
+    
+    # scritte che compaiono quando prov a chiudere il gioco
+    scritta_fine_gioco = font_titolo.render('Hai chiuso il gioco!', True, 'white')
+    scritta_chiusura_gioco = font_normale.render('premi ESC per uscire del gioco', True, 'white')
 
-    screen = pygame.display.set_mode( (800, 600) )
+    # genera una scharmata con il titolo in alto 
+    screen = pygame.display.set_mode( (altezza_schermata, lunghezza_schermata) )
     pygame.display.set_caption("Il mio primo gioco con PyGame!")
 
     running = True
 
     while running:
+        # serve a gestire la chiusura della pagina del gioco
         for event in pygame.event.get():
+            
             if event.type == pygame.QUIT:
                 running = False
-            # Se l'evento è la pressione di un tasto...
-            # ... e il tasto è il tasto ESC.. esc(i)!
+            
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
 
-    screen.fill("green")
-    pygame.display.flip()
+        screen.fill("red")
+        screen.blit(scritta_fine_gioco, (100,100))
+        screen.blit(scritta_chiusura_gioco, (100,300))
+        pygame.display.flip()
 
     pygame.quit()
 
