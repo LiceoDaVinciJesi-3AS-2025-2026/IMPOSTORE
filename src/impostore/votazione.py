a72da51a1d5a3694340a11dffc0f243d8aa8af43
TommasoPalmucci <palmuccitommy09.gm@gmail.com>
2026-02-25 12:45:24 +0100

    controllare impostori

--- src/impostore/votazione.py
+++ src/impostore/votazione.py
@@ -1,6 +1,8 @@
 from lista_partecipanti import listaPartecipanti
+from lista_partecipanti import impostore
 
 lista_partecipanti = listaPartecipanti()
+impostori = impostore()
 
 def votazioni():
     '''La funzione permette ai giocatori di decidere chi eliminare'''
@@ -26,6 +28,7 @@ def contaVoti(lista_voti):
 
     giocatore_da_eliminare = ""
     massimo_voti = 0
+    lista_eliminati = []
 
     for nome in lista_voti:
         conteggio_attuale = lista_voti.count(nome)
@@ -36,9 +39,24 @@ def contaVoti(lista_voti):
 
     while giocatore_da_eliminare in lista_voti:
         lista_voti.remove(giocatore_da_eliminare)
+        lista_eliminati.append(giocatore_da_eliminare)
 
     
-    return lista_voti
+    return lista_eliminati
+
+def controllaImpostori(lista_eliminati):
+    ''' La funzione controlla se sono stati eliminati gli impostori'''
+
+    for nome in lista_eliminati:
+        if nome in impostori:
+            print(nome, "era un impostore")
+        else:
+            print("non sono stati eliminati degli impostori")
+    
+        lista_partecipanti.remove(nome)
+
+    return lista_partecipanti
+
 
 
 
